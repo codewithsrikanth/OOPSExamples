bea14b7633621d35f2dccf5a60061bf9941457a7
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-02-27 20:17:45 +0530

    Add project files.

---
+++ CAConstructors/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CAConstructors
+{
+    class Employee
+    {
+        int empId;string empName;double empSalary;
+        //User-Defined Default Constructor
+        public Employee()
+        {
+            empId = 101;
+            empName = "Srikanth";
+            empSalary = 12345;
+        }
+        //Paramterized Constructor
+        public Employee(int id,string name,double salary)
+        {
+            empId = id;
+            empName = name;
+            empSalary = salary;
+        }
+        //Function/Method
+        public void Display()
+        {
+            Console.WriteLine($"EmpID is: {empId}, EmpName is: {empName} and Salary: {empSalary}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Display();
+            Employee emp2 = new Employee(102, "Diyansh", 98765);
+            emp2.Display();
+            Employee emp3 = new Employee(103, "Raju", 96858);
+            emp3.Display();
+        }
+    }
+}
---
+++ CAOOPSEx/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CAOOPSEx
+{
+    class Product
+    {
+        //variables
+        int productId; string productName; double price; int qty;
+
+        //Method
+        public void GetProductInfo()
+        {
+            Console.WriteLine("Enter Product Details: ");
+            productId = Convert.ToInt32(Console.ReadLine());
+            productName = Console.ReadLine();
+            price = Convert.ToDouble(Console.ReadLine());
+            qty = Convert.ToInt32(Console.ReadLine());
+        }
+        //Function
+        public double GetPrice()
+        {
+            return price * qty;
+        }
+        //Method
+        public void Display()
+        {
+            double total = GetPrice();
+            Console.WriteLine("Product Name is: "+productName);
+            Console.WriteLine("Product Cost is: "+total);
+        }
+        //Function
+        public string Greet(string name)
+        {
+            double x = Math.Sqrt(10);
+            Console.WriteLine(x);
+            return "Welcome " + name;
+        }
+    }
+    class Example1
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            //p1.GetProductInfo();
+            //p1.Display();
+            string msg = p1.Greet("Srikanth");
+            Console.WriteLine(msg);
+        }
+    }
+}
